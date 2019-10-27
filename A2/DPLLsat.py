67388fa8d232ee88b257cd9be5b358c81555496e
Deniz Evrendilek <devrendilek@gmail.com>
2019-10-26 22:30:07 -0700

    DPLL with nested loop

--- A2/DPLLsat.py
+++ A2/DPLLsat.py
@@ -116,22 +116,150 @@ def main(argv):
 #  DPLLsat(), DPLL(), pure-elim(), propagate-units(), and
 #  any other auxiliary functions
 def solve_dpll(instance, verbosity):
-    print(instance)
-    # instance.VARS goes 1 to N in a dict
-    print(instance.VARS)
-    print(verbosity)
+    # print(instance)
+    # # instance.VARS goes 1 to N in a dict
+    # print(instance.VARS)
+    # print(verbosity)
     ###########################################
     # Start your code
-    clauses = instance.clauses
-    variables = instance.VARS
+    clauses = copy.deepcopy(instance.clauses)
+    clausess = copy.deepcopy(instance.clauses)
+    variables = copy.deepcopy(instance.VARS)
+
+    symbols = copy.deepcopy(variables)
+    model = {}
+
+    def DPLLsat(clauses, symbols, model):
+        # if len(clauses) == 0:
+        #     return model
+
+        # if clausesNotValid(clauses):
+        #     return False
+
+        # clauses = pure_elim(clauses,symbols, model)
+        # alpha = unitProp(clauses)
+        # # print("ALPHA: ", alpha)
+        # if alpha == False:
+        #     return False
+        # if len(alpha) == 0:
+        #     return model
+
+        # P = symbols.pop()
+        # for value in [0,1]:
+
+        for i in range(len(symbols)):
+            for j in range(len(symbols)):
+                model.update({j+1:i%2})
+                clauses = pure_elim(clauses,symbols, model)
+                alpha = unitProp(clauses)
+                alpha = simplify(clauses, model)
+                ret, truth = alpha
+                if truth is not False:
+                    return model
+        return False
+
+        # model.update({P:1})
+        # alpha = simplify(clauses, model)
+        # print("alpha: ", alpha)
+        # ret = DPLLsat(clauses, symbols, model)
+        # if ret is not False:
+        #     return ret
+        # return DPLLsat(clauses, variables, model)
+    
+
+    def unitProp(clauses):
+        # print("HELLO")
+        if clausesNotValid(clauses):
+            return False
+        l = [clause[0] for clause in clauses if len(clause)==1]
+        if len(l) == 0:
+            return clauses
+        # print("Begining L: ", l)
+        while len(l) != 0:
+            # print("L: ", l)
+            for literal in l: 
+                for clause in reversed(clauses):
+                    if len(clause) == 0:
+                        return False
+                    if len(clause) != 1 and literal in clause:
+                        clauses.remove(clause)
+                    if len(clause) != 1 and -literal in clause:
+                        clause.remove(-literal)
+            old_l = l
+            l = [clause[0] for clause in clauses if len(clause)==1]
+            if l == old_l:
+                l = []
+        return clauses, True
+
+    def pure_elim(clauses, symbols, model):
+        pos = [var for clause in clauses for var in clause if var > 0]
+        pos = list(dict.fromkeys(pos))
+        neg = [var for clause in clauses for var in clause if var < 0]
+        neg = list(dict.fromkeys(neg))
+        vars = []
+        for x in neg:
+            if abs(x) not in pos:
+                vars.append(x)
+        for x in pos:
+            if -x not in neg:
+                vars.append(x)
+        for symbol in vars:
+            if symbol > 0: model.update({symbol:1})
+            if symbol < 0: model.update({-symbol:0})
+            for clause in reversed(clauses):
+                if symbol in clause:
+                    clauses.remove(clause)
+        return clauses
 
 
+    def simplify(clauses, model):
+        # print("B4: ", clauses)
+        # print("Model: ",model)
+        simplifiedClauses = copy.deepcopy(clauses)
+        for literal, truthValue in model.items():
+            for clause in reversed(simplifiedClauses):
+                # print("clause for +: ", clause)
+                if literal in clause:
+                    if truthValue == 1: simplifiedClauses.remove(clause)
+                    if truthValue == 0: clause.remove(literal)
+            for clause in reversed(simplifiedClauses):
+                if -literal in clause:
+                    if truthValue == 1: clause.remove(-literal)
+                    if truthValue == 0: simplifiedClauses.remove(clause)
+        # print("SIMPLIFIED: ", simplifiedClauses)
+        if len(simplifiedClauses) == 0:
+            return (simplifiedClauses, True)
+        return (simplifiedClauses, False)
 
+    def clausesNotValid(clauses):
+        if any(len(clause) == 0 for clause in clauses):
+            return True
+        else:
+            return False
 
+    callF = DPLLsat(clauses, variables, model)
+    # callF = DPLL(clauses, variables, model)
+    # print("CallF: ", callF)
 
+    if callF is None:
+        print("Returned NULL")
+    if callF is False:
+        print("UNSAT")
+    if verbosity is True and callF is not False and callF is not None:
+        output = []
+        for symbol, truthValue in callF.items():
+            if truthValue == 1:
+                output.append(symbol)
+        print("SAT")
+        output.sort()
+        print(output)
+        
+    if verbosity is False and callF is not False and callF is not None:
+        print("SAT")
 
+    # if verbosity is False and callF is False:
+    #     print("UNSAT")
     ###########################################
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
